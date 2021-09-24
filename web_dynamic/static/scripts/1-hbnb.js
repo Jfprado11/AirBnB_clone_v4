189dcfb733c5c63b7246c124935d8ca4bedfd37e
Felipe Prado <jfpc11@misena.edu.co>
2021-09-23 22:13:53 -0500

    adding a new dinammic contents for amenitites

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,16 @@
-$(function() {
-	let list_amenity_id = []
-    $('input:checkbox').click(function () {
-		if($(this).is(':checked')) {
-			list_amenity_id.push($(this).attr('data-id'))
-			console.log(list_amenity_id)
-		}
-		else {
-			list_amenity_id.splice(list_amenity_id.indexOf($(this).attr('data-id')), 1)
-			console.log(list_amenity_id)
-		}
-	})
+$(function () {
+  const listAmenityId = [];
+  const listAmenityName = [];
+  $("input:checkbox").click(function () {
+    if ($(this).is(":checked")) {
+      listAmenityId.push($(this).attr("data-id"));
+      listAmenityName.push($(this).attr("data-name"));
+      console.log(listAmenityId);
+    } else {
+      listAmenityId.splice(listAmenityId.indexOf($(this).attr("data-id")), 1);
+      listAmenityName.splice(listAmenityName.indexOf($(this).attr("data-name")), 1);
+      console.log(listAmenityId);
+    }
+    $(".amenities h4").text(listAmenityName.join(", "));
+  });
 });
